a279982cf7bcb48444eed46c66757c1cfac62202
Sreerag V S <svs9745@gmail.com>
2021-06-17 21:15:55 +0530

    ........

--- app/src/main/java/com/bot/ksrtctrack/MainActivity.java
+++ app/src/main/java/com/bot/ksrtctrack/MainActivity.java
@@ -3,13 +3,28 @@ package com.bot.ksrtctrack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnStart, btnStop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart = findViewById(R.id.start_button);
+        btnStop = findViewById(R.id.stop_button);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
