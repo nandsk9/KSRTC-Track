57f8fb82108827261bd65ae0dcc78d419586370e
Sreerag V S <svs9745@gmail.com>
2021-06-29 13:18:26 +0530

    Google map markers added

--- app/src/main/java/com/bot/ksrtctrack/MainActivity.java
+++ app/src/main/java/com/bot/ksrtctrack/MainActivity.java
@@ -1,19 +1,40 @@
 package com.bot.ksrtctrack;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     Button btnStart, btnStop;
+    GoogleMap ggleMap;
+    MapView mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnStart = findViewById(R.id.start_button);
         btnStop = findViewById(R.id.stop_button);
+        mapView = findViewById(R.id.mapView1);
+        mapView.onCreate(null);
+        mapView.getMapAsync(this);
+        mapView.setEnabled(true);
+
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,5 +47,34 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        ggleMap = googleMap;
+        ggleMap.setMinZoomPreference(10);
+        LatLng ll1 = new LatLng(12.3140248,75.0875445);
+        ggleMap.animateCamera(CameraUpdateFactory.newLatLng(ll1));
+        ggleMap.addMarker(new MarkerOptions().position(ll1).title("Kanhagad Bus Stand"));
+        LatLng ll2 = new LatLng(12.411063099999993,75.0225179);
+        ggleMap.animateCamera(CameraUpdateFactory.newLatLng(ll2));
+        ggleMap.addMarker(new MarkerOptions().position(ll2).title("Thrikkanand").icon(getBitMap(this,R.drawable.bus_marker)));
+        LatLng ll3 = new LatLng(12.4999548,74.9819596);
+        ggleMap.animateCamera(CameraUpdateFactory.newLatLng(ll3));
+        ggleMap.addMarker(new MarkerOptions().position(ll3).title("Kasaragod Bus Stand"));
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    private BitmapDescriptor getBitMap(Context context, int resId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, resId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 }
